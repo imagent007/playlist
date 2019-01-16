ef11548b9f9c9984588b3891e8561786a0e1b03e
Vaughn <vgree007@gmail.com>
2019-01-16 22:45:46 +0000

    first push

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["Look Alive" , "Sicko Mode" , "Plug Walk" , "Exchange"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
+var imagesLinks =  ["https://i.ytimg.com/vi/ue1czxmU7_s/maxresdefault.jpg" , "https://i.ytimg.com/vi/d-JBBNg8YKs/maxresdefault.jpg" , "" , ""]
+var artists = ["BlocBoy JB" , "Travis Scott" , "" , ""]
+var songLengths = ["3:14" , "5:14" , "" , ""]
+var songLinks = ["https://youtu.be/NV-3s2wwC8c" , "https://youtu.be/d-JBBNg8YKs" , "" , ""]
+
 // Make sure they match the same order as your array above
 
 
